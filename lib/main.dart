8d3bad7a2fb13a56a1aa2eb8344d2168669d0ca6
firstquarterlee <firstquarterlee@gmail.com>
2024-07-02 22:38:18 +0900

    숙제 제출

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +10,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container( //함수의 파라미터같은거다
-        width: 50, //파라미터다!! 위드스 파라미터에는 50을 //그리고 단위는 LP다
-        height: 50, //하이트 파라미터에는 50을
-        color: Colors.blue, //컬러 파라미터에는 블루를
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Text('앱임'),
+            ],
+          ),
+        ),
+        body: Text('안녕'),
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.phone),
+            Icon(Icons.phone),
+          ],
+        ),
       ),
     );
-
   }
 }
