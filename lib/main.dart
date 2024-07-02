82ec3c935bec42309de09b4671ae02be303555ca
firstquarterlee <firstquarterlee@gmail.com>
2024-07-03 00:42:51 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,19 +18,15 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 900,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
+        body: Container(
+          width: 150, height: 50,
+          //margin: EdgeInsets.all(20), //마진을 주고싶다?(바깥여백)
+          //padding: EdgeInsets.all(20), //패딩
+          //margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black)
           ),
+          child: Text('dddddd'),
         ),
       ),
     );
