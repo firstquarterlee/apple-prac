b12ea3c62a3a29d715335a7fa2c40c00d8ef7e47
firstquarterlee <firstquarterlee@gmail.com>
2024-07-03 17:29:02 +0900

    Expanded, Flexible

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Row(
+          appBar: AppBar(
+            title: Row(
+              children: [
+                Text('앱임'),
+              ],
+            ),
+          ),
+          body: Row(
             children: [
-              Text('앱임'),
+              Expanded(child: Container( color: Colors.blue,)),
+              Container(width: 100, color: Colors.green),
             ],
-          ),
-        ),
-        body: Container(
-          width: 150, height: 50,
-          //margin: EdgeInsets.all(20), //마진을 주고싶다?(바깥여백)
-          //padding: EdgeInsets.all(20), //패딩
-          //margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black)
-          ),
-          child: Text('dddddd'),
-        ),
-      ),
+          )),
     );
   }
 }
