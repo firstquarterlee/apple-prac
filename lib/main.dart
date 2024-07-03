74fdf752f9f6cc41920d97f2db0bcbd2374eb81c
firstquarterlee <firstquarterlee@gmail.com>
2024-07-03 20:08:32 +0900

    커스텀 위젯

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,34 +12,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Container(
-          padding: EdgeInsets.all(20),
-          height: 150,
-          child: Row(
-            children: [
-              Image.asset('assets/korea.jpeg', width: 150,),
-              Flexible(
-                flex: 3,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라팝니다.'),
-                    Text('금호동 3가.'),
-                    Text('7000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4'),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: ShopItem(),
       ),
+    );
+  }
+}
+
+///커스텀 위젯 만듬
+class ShopItem extends StatelessWidget {
+  const ShopItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text('안녕'),
     );
   }
 }
