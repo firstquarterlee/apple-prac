671cab62abbcbd934830b7d7256d1f131233378a
firstquarterlee <firstquarterlee@gmail.com>
2024-07-03 17:44:23 +0900

    상품 레이아웃 숙제

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Row(
-              children: [
-                Text('앱임'),
-              ],
-            ),
-          ),
-          body: Row(
+        appBar: AppBar(),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          height: 150,
+          child: Row(
             children: [
-              Expanded(child: Container( color: Colors.blue,)),
-              Container(width: 100, color: Colors.green),
+              Image.asset('assets/korea.jpeg', width: 150,),
+              Flexible(
+                flex: 3,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('카메라팝니다.'),
+                    Text('금호동 3가.'),
+                    Text('7000원'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('4'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
