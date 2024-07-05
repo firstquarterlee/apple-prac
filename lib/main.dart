99e9be3fa3c91fa6805c84f6b900b7b2f84a95c3
firstquarterlee <firstquarterlee@gmail.com>
2024-07-05 21:32:45 +0900

    setState 숙제

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var a = 1;
   var name = ['김영숙', '홍길동', '피자집'];
+  var like = [0, 0, 0];
 
   //데이터 잠깐 저장하는 곳 : 변수
 
@@ -21,22 +22,20 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            floatingActionButton: FloatingActionButton(
-                child: Text(a.toString()),
-                onPressed: () {
-                  setState(() {
-                    a++; //state가 변경되면 재 랜더링됨!!! 그래서 숫자가 바뀜
-                  });
-                }),
             appBar: AppBar(),
             body: ListView.builder(
               itemCount: 3, // 몇번 반복할건지
               itemBuilder: (context, i) {
                 // 함수 넣기
-                print(i);
                 return ListTile(
-                  leading: Image.asset('assets/korea.jpeg'),
-                  title: Text(name[1]),
+                  leading: Text(like[i].toString()),
+                  title: Text(name[i]),
+                  trailing: TextButton(
+                    child: Text('좋아요'),
+                    onPressed: () {
+                      setState(() {like[i]++;});
+                    },
+                  ),
                 );
               },
             )));
