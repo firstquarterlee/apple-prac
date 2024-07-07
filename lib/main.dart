7b403d80c3ff02c421a3c4f2e371c921a71806fc
firstquarterlee <firstquarterlee@gmail.com>
2024-07-07 20:17:47 +0900

    state 전송 방법

--- lib/main.dart
+++ lib/main.dart
@@ -12,45 +12,40 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
+  var a = 3;
+  var prac = 7;
   var name = ['김영숙', '홍길동', '피자집'];
   var like = [0, 0, 0];
 
   //데이터 잠깐 저장하는 곳 : 변수
 
   @override
-  build(context) { //Scaffold() 부모가 누군지 알려줌
+  build(context) {
+    //Scaffold() 부모가 누군지 알려줌
     return Scaffold(
-      floatingActionButton: Builder(
-        builder: (context) {
-          return FloatingActionButton(
-            onPressed: () {
-              print(context.findAncestorWidgetOfExactType<MaterialApp>());
-              //부모중에 MaterialApp 있는지 찾아주세요~ 라는 간단한 함수임
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return Dialog(
-                      child: Text('안녕'),
-                    );
-                  });
-            },
-          );
-        }
-      ),
       appBar: AppBar(),
       body: ListView.builder(
         itemCount: 3,
         itemBuilder: (c, i) {
           return ListTile(
-            leading: Image.asset('korea.jpeg'),
+            leading: Image.asset(
+              'korea.jpeg',
+              width: 100,
+            ),
             title: Text(name[i]),
           );
         },
       ),
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return DialogUI(state : a, state2 : prac);
+              });
+        },
+      ),
     );
-
   }
 }
 
@@ -60,5 +55,43 @@ class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+}
+
+class DialogUI extends StatelessWidget {
+  const DialogUI({Key? key, this.state, this.state2}) : super(key: key);
+  final state;
+  final state2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField(),
+            TextButton(
+              child:
+                Column(
+                  children: [
+                    Text(state.toString()),
+                    Text(state.toString()),
+                  ],
+                ),
+
+              onPressed: () {},
+            ),
+            TextButton(
+              child: Text('취소'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
