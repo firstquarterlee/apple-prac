d2befdb6d47da857ee6abe57277d6e54e006164c
이상현 <firstquarterlee@gmail.com>
2024-07-12 21:10:32 +0900

    The End

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:contacts_service/contacts_service.dart';
 
 void main() {
   runApp(MaterialApp(home: MyApp()));
@@ -12,10 +14,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  getPermission() async {//패키지만든사람이 정한 사용법일 뿐
+    var status = await Permission.contacts.status;
+    if (status.isGranted) {
+      print('허락됨');
+      var contacts = await ContactsService.getContacts();
+      print(contacts);
+      setState(() {
+        name = contacts;
+      });
+
+      //print(contacts[1].displayName);
+      //var newPerson = new Contact();//class에서 인스턴스뽑는 문법 그럴때 new 키워드 씀 근데 생략 가능
+      //newPerson.givenName = '민수';
+
+    } else if (status.isDenied) {
+      print('거절됨');
+      Permission.contacts.request();//팝업창 띄워주세요
+      //openAppSettings(); //앱설정화면 켜줌(거절당하면 유저가 앱설정 들어가서 직접 권한 켜야함)
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPermission();
+  }
+
   var total = 3; //state 만듬
   var a = 3;
   var prac = 7;
-  var name = ['김영숙', '홍길동', '피자집'];
+  List<Contact> name = [];
   var like = [0, 0, 0];
 
   addName(a) {
@@ -39,17 +69,19 @@ class _MyAppState extends State<MyApp> {
     //Scaffold() 부모가 누군지 알려줌
     return Scaffold(
       appBar: AppBar(
-        title: Text(total.toString()),
+        title: Text(total.toString()), actions: [
+          IconButton(onPressed: (){ getPermission();}, icon: Icon(Icons.contacts))
+      ],
       ),
       body: ListView.builder(
         itemCount: name.length,
         itemBuilder: (c, i) {
           return ListTile(
             leading: Image.asset(
-              'korea.jpeg',
+              'assets/korea.jpeg',
               width: 100,
             ),
-            title: Text(name[i]),
+            title: Text(name[i].givenName ?? '이름이없는놈'),
           );
         },
       ),
